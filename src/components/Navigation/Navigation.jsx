4bb5a7ce3ccc2b5bb680b2b739c103a47cc36d35
Vladimir Tryasunov <tryasunov933@gmail.com>
2025-02-03 10:34:20 +0200

    fix navigation

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,7 +1,11 @@
 import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { selectIsLoggedIn } from "../../redux/auth/selectors";
 import { Button, Box } from "@mui/material";
 
 const Navigation = () => {
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+
   return (
     <Box sx={{ display: "flex", gap: 2 }}>
       <Button
@@ -16,18 +20,21 @@ const Navigation = () => {
       >
         Home
       </Button>
-      <Button
-        component={NavLink}
-        to="/contacts"
-        color="inherit"
-        sx={{
-          "&.active": {
-            borderBottom: "2px solid white",
-          },
-        }}
-      >
-        Contacts
-      </Button>
+
+      {isLoggedIn && (
+        <Button
+          component={NavLink}
+          to="/contacts"
+          color="inherit"
+          sx={{
+            "&.active": {
+              borderBottom: "2px solid white",
+            },
+          }}
+        >
+          Contacts
+        </Button>
+      )}
     </Box>
   );
 };
